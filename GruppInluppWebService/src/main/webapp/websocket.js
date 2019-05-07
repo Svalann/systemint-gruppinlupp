ae754f4d55002add03efb0b356f1425ed593522b
Viktor Svahlstedt <viktor.svahlstedt@gmail.com>
2019-05-07 14:31:32 +0200

    lagt till humidity, created. Skriver ut på HTML

--- GruppInluppWebService/src/main/webapp/websocket.js
+++ GruppInluppWebService/src/main/webapp/websocket.js
@@ -6,11 +6,25 @@ function connect() {
     ws.onmessage = function(event) {
         
         var message = JSON.parse(event.data);
-        var tempText = document.getElementById("txttemperature"); 
+        
+        var tempText = document.getElementById("txtTemperature"); 
+        var humidText = document.getElementById("txtHumidity"); 
+        var createdText = document.getElementById("txtCreated"); 
+        var updatedText = document.getElementById("txtLastUpdate"); 
+        
         
         if (message.hasOwnProperty("temperature")){     
             tempText.textContent = message.temperature;
         }
+        if (message.hasOwnProperty("humidity")){     
+            humidText.textContent = message.humidity;
+        }
+        if (message.hasOwnProperty("created")){     
+            createdText.textContent = message.created;
+        }
+        var today = new Date(); 
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        updatedText.textContent = time;
     };
 }
 
