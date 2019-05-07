4e9f9072195495ffb05614caf05a5a9d4392acef
olgamedt <andicsson@gmail.com>
2019-05-07 10:08:27 +0200

    Temperature/ humidity klassen

---
+++ GruppInluppWebService/src/test/java/TempClass.java
@@ -0,0 +1,46 @@
+
+import java.io.Serializable;
+
+public class TempClass implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private float temperature;
+    private float humidity;
+
+    public TempClass() {
+    }
+
+    public TempClass(int id, float temperature, float humidity) {
+
+        this.id = id;
+        this.temperature = temperature;
+        this.humidity = humidity;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+    
+}
