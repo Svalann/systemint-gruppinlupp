87944239a1f063b45b2c46f3d21e2ddc11a84f2f
Viktor Svahlstedt <viktor.svahlstedt@gmail.com>
2019-05-10 11:36:13 +0200

    ändrar tiden från databasen +2. Lagt in bootstrap i html

--- GruppInluppWebService/src/main/webapp/websocket.js
+++ GruppInluppWebService/src/main/webapp/websocket.js
@@ -30,5 +30,36 @@ function connect() {
 }
 
 function getAllData(){
+    var array = arrayFromDB;
     
+    graf.DataPoints = array;
+    
+    
+    const request = new XMLHttpRequest();
+ 
+    request.open('GET', '/GruppInluppWebService/rest/SensorDataService/TempSensor/getAllData');
+    request.send(); 
+ 
+    request.onload = () => {
+      if (request.status === 200) {
+        console.log("Success"); // So extract data from json and create table
+
+        //Extracting data
+        var jokeid = JSON.parse(request.response).value.id;
+        var joke = JSON.parse(request.response).value.joke;
+
+        //Creating table
+        var table="<table>";
+            table+="<tr><td>Joke ID</td><td>Joke</td></tr>"; 
+            table+="<tr><td>"+jokeid+"</td><td>"+joke+"</td></tr>";
+            table+="</table>";
+
+        //Showing the table inside table
+        document.getElementById("mydiv").innerHTML = table;   
+        } 
+      };
+
+      request.onerror = () => {
+        console.log("error")
+      };
 }
